bcb22bc3ea513e2bb868d73040199d7534f028bc
afreen shaik <affushaik9493@gmail.com>
2023-09-07 00:24:15 +0530

    implement correct_name

--- person.rb
+++ person.rb
@@ -1,8 +1,12 @@
 class Person
+  require './nameable.rb'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = generate_id
     @name = name
     @age = age
@@ -12,6 +16,11 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
+
 
   private
 
