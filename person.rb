c7da5c9a7435d6cea636661808748039e9269467
afreen shaik <affushaik9493@gmail.com>
2023-09-07 01:02:45 +0530

    solve error

--- person.rb
+++ person.rb
@@ -1,36 +1,34 @@
-class Person
-  require './nameable.rb'
+require './nameable'
 
-  class Person < Nameable
-    attr_accessor :name, :age
-    attr_reader :id
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      super()
-      @id = generate_id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-      of_age? || @parent_permission
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def correct_name
-      @name
-    end
+  def correct_name
+    @name
+  end
 
 
-    private
+  private
 
-    def of_age?
-      @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 
-    def generate_id
-      # Code to generate a unique ID
-      rand(1..1000)
-    end
+  def generate_id
+    # Code to generate a unique ID
+    rand(1..1000)
   end
 end
