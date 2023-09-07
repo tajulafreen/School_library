71712597729428a57106383179bc96591cfee4da
afreen shaik <affushaik9493@gmail.com>
2023-09-07 23:38:55 +0530

    add person class

--- student.rb
+++ student.rb
@@ -1,7 +1,14 @@
+require './person.rb'
 class Student < Person
+  attr_accessor :classroom
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.includes?(self)
   end
 
   def play_hooky
