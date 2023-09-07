95774a04e1c895288402775e1ddc8b925783e83e
afreen shaik <affushaik9493@gmail.com>
2023-09-07 23:41:51 +0530

    solve linter errors

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
-require './person.rb'
+require './person'
 class Student < Person
   attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
